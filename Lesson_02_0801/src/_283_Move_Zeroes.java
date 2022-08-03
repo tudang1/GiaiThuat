e5d48d4c7603ab0a7944843467222aa36345b7c9
Tudang1 <teedangtu@gmail.com>
2022-08-03 23:00:38 +0700

    Bài Tập Buổi 2

--- Lesson_02_0801/src/_283_Move_Zeroes.java
+++ Lesson_02_0801/src/_283_Move_Zeroes.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 public class _283_Move_Zeroes {
-    public void moveZeroes(int[] nums) {
+    public static void moveZeroes(int[] nums) {
         int m = 0;
         for(int i =0; i < nums.length; i++){
             if(nums[i] != 0){
@@ -10,6 +12,14 @@ public class _283_Move_Zeroes {
         while(m < nums.length){
             nums[m] = 0;
             m++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        moveZeroes(nums);
+        for (int i: nums){
+            System.out.println(i);
         }
     }
 }
